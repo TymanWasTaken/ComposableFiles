b0479efeb928dcd551bb038ecb7b4e3c1cdedb57
Tyler Beckman <tbeckman530@gmail.com>
2022-05-24 17:33:01 -0600

    remove sftp code temporarily, I have better priorities

--- app/src/main/java/tech/tyman/composablefiles/ui/screens/Main.kt
+++ app/src/main/java/tech/tyman/composablefiles/ui/screens/Main.kt
@@ -2,24 +2,13 @@ package tech.tyman.composablefiles.ui.screens
 
 import android.os.Environment
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 import tech.tyman.composablefiles.data.filesystems.LocalFileSystem
-import tech.tyman.composablefiles.data.filesystems.SftpFileSystem
 import tech.tyman.composablefiles.ui.components.DirectoryComponent
-import tech.tyman.composablefiles.utils.showToast
 
 @Composable
 fun MainScreen() {
-    val context = LocalContext.current
     DirectoryComponent(
         path = Environment.getExternalStorageDirectory().absolutePath,
-        SftpFileSystem(
-            username = "tyman",
-            host = "192.168.0.98",
-            port = 22,
-            onPasswordPrompt = { "LinuxIsCool7!" },
-            onMessage = { context.showToast(it) },
-            onConfirmationPrompt = { context.showToast(it); true },
-        )
+        LocalFileSystem()
     )
 }
