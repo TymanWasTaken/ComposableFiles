403d887b8364c61daf07b4ab18781c6d936f86e3
TymanWasTaken <tbeckman530@gmail.com>
2022-05-26 22:58:20 -0600

    Migrate to kotlin gradle dsl

---
+++ app/build.gradle.kts
@@ -0,0 +1,112 @@
+import java.io.File
+import java.io.FileInputStream
+import java.util.*
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = 32
+
+    defaultConfig {
+        applicationId = "tech.tyman.composablefiles"
+        minSdk = 21
+        targetSdk = 32
+
+        val appVersionCode: Int by rootProject.extra
+        val appVersion: String by rootProject.extra
+
+        versionCode = appVersionCode
+        versionName = appVersion
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    if (System.getenv().get("CI") != "true") {
+        // Create a variable called keystorePropertiesFile, and initialize it to your
+        // keystore.properties file, in the rootProject folder.
+        val keystorePropertiesFile = rootProject.file("keystore.properties")
+
+        // Initialize a new Properties() object called keystoreProperties.
+        val keystoreProperties = Properties()
+
+        // Load your keystore.properties file into the keystoreProperties object.
+        keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+
+        val releaseKeyAlias: String by keystoreProperties
+        val releaseKeyPassword: String by keystoreProperties
+        val releaseStoreFile: String by keystoreProperties
+        val releaseStorePassword: String by keystoreProperties
+
+        signingConfigs {
+            create("release") {
+                keyAlias = releaseKeyAlias
+                keyPassword = releaseKeyPassword
+                storeFile = file(releaseStoreFile)
+                storePassword = releaseStorePassword
+
+                enableV1Signing = true
+                enableV2Signing = true
+                enableV3Signing = true
+                enableV4Signing = true
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            if (System.getenv().get("CI") != "true") {
+                signingConfig = signingConfigs.getByName("release")
+            }
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        val composeVersion: String by rootProject.extra
+        kotlinCompilerExtensionVersion = composeVersion
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    val composeVersion: String by rootProject.extra
+    val coilVersion: String by rootProject.extra
+
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.material3:material3:1.0.0-alpha12")
+    implementation("androidx.compose.material:material-icons-extended:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
+    implementation("androidx.activity:activity-compose:1.4.0")
+    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:$composeVersion")
+
+    implementation("io.coil-kt:coil:$coilVersion")
+    implementation("io.coil-kt:coil-compose:$coilVersion")
+    implementation("io.coil-kt:coil-gif:$coilVersion")
+    implementation("io.coil-kt:coil-svg:$coilVersion")
+    implementation("io.coil-kt:coil-video:$coilVersion")
+
+//    implementation("com.github.mwiede:jsch:0.2.1")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,4 +13,4 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "ComposableFiles"
-include ':app'
+include(":app")
